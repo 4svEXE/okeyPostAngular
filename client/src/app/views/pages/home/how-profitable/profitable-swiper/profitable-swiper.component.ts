335ca44b682a1a2660fca9c427180ff49d97f0c9
Иван Сташко <4sv.exe@gmail.com>
2023-07-08 15:51:03 +0300

    swiper in progress

--- client/src/app/views/pages/home/how-profitable/profitable-swiper/profitable-swiper.component.ts
+++ client/src/app/views/pages/home/how-profitable/profitable-swiper/profitable-swiper.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-profitable-swiper',
-  templateUrl: './profitable-swiper.component.html',
-  styleUrls: ['./profitable-swiper.component.scss']
+  selector: "app-profitable-swiper",
+  templateUrl: "./profitable-swiper.component.html",
+  styleUrls: ["./profitable-swiper.component.scss"],
 })
 export class ProfitableSwiperComponent {
-
+  images = [{ path: "assets/home/firstScreen.svg" }];
 }
