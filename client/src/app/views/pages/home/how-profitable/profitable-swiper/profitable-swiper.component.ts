1cd5b8541acadbb27cb51bd611d602adcb67615a
Иван Сташко <4sv.exe@gmail.com>
2023-07-10 11:55:01 +0300

    swiper carousel in progress

--- client/src/app/views/pages/home/how-profitable/profitable-swiper/profitable-swiper.component.ts
+++ client/src/app/views/pages/home/how-profitable/profitable-swiper/profitable-swiper.component.ts
@@ -6,6 +6,69 @@ import { Component } from "@angular/core";
   styleUrls: ["./profitable-swiper.component.scss"],
 })
 export class ProfitableSwiperComponent {
-  screenWidth = window.innerWidth
-  swiperHeight: number = 600;
+  screenWidth = window.innerWidth;
+  swiperHeight: number = 1000;
+
+  currentCategoty: string = "clothes";
+  products: any[] = [
+    {
+      name: "Columbia Barlow Pass 550 Turbodown Jacket",
+      image: "assets/home/HowProfitable/swiper/products/ColumbiaBarlow.png",
+      priceUk: 80.62,
+      priceUa: 6399,
+      economy: 200,
+      delivery: 10,
+      category: "clothes",
+    },
+    {
+      name: "Columbia Barlow Pass 550 Turbodown Jacket",
+      image: "assets/home/HowProfitable/swiper/products/ColumbiaBarlow.png",
+      priceUk: 8220,
+      priceUa: 6399,
+      economy: 200,
+      delivery: 10,
+      category: "clothes",
+    },
+    {
+      name: "Columbia Barlow Pass 550 Turbodown Jacket",
+      image: "assets/home/HowProfitable/swiper/products/ColumbiaBarlow.png",
+      priceUk: 80,
+      priceUa: 6399,
+      economy: 200,
+      delivery: 10,
+      category: "clothes",
+    },
+    {
+      name: "Columbia Barlow Pass 550 Turbodown Jacket",
+      image: "assets/home/HowProfitable/swiper/products/ColumbiaBarlow.png",
+      priceUk: 80,
+      priceUa: 6399,
+      economy: 200,
+      delivery: 10,
+      category: "clothes",
+    },
+  ];
+
+  filteredProducts = this.products.filter(
+    (p: any) => p.category === this.currentCategoty
+  );
+
+  currentSlide: number = 1;
+
+  onResize(event: any): void {
+    this.screenWidth = event.target.innerWidth;
+    this.responciveSwiper();
+  }
+
+  responciveSwiper(): void {
+    if (this.screenWidth > 1020) {
+      this.swiperHeight = 350;
+    } else {
+      this.swiperHeight = 1000;
+    }
+  }
+
+  ngOnInit() {
+    this.responciveSwiper();
+  }
 }
